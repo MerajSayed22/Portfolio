2e2168adf2286eef52c5417d02376f9a351d8b6d
Meraj <mrj.ahmed22@gmail.com>
2025-01-11 02:11:38 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+
+    <div className='bg-olive h-12 my-10 mx-5 rounded-3xl'>
+    <div className='flex justify-start'>
+    <p className='bg-yellowMix text-3xl rounded-3xl inline-block mx-3 my-1.5 w-10 text-center text-gray-600'>M</p>
+    <h1 className='x-10 inline-block text-white'>Meraj Sayed </h1>
+    </div>
+      
+    
+    </div>
+
+
+
+
+    </>
+    
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple:'#6A80B9',
+        cream:'#FFF6B3',
+        skin:'#F6C794',
+        olive:'#344C36',
+        yellowMix:'#F9AD1B',
+      }
+    },
+  },
+  plugins: [],
+}
+
